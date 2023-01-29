b41266924c7cb17c07e6ab2ea09d7fbd8c910928
Colaborador <colaborador@N094693.local>
2023-01-29 18:39:40 -0300

    feat/ validation pipe

--- src/despesas/despesas.controller.ts
+++ src/despesas/despesas.controller.ts
@@ -1,13 +1,24 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { DespesasService } from './despesas.service';
 import { CriarAtualizarDespesaDto } from './dtos/criar-atualizar-despesa.dto';
 import { Despesa } from './interfaces/despesa.interface';
+import { DespesasValidacaoPipe } from './pipes/despesas-validacao.pipe';
 
 @Controller('despesas')
 export class DespesasController {
   constructor(private readonly despesasService: DespesasService) {}
 
   @Post()
+  @UsePipes(ValidationPipe)
   async criarDespesa(@Body() criarDespesaDto: CriarAtualizarDespesaDto) {
     return await this.despesasService.criarDespesa(criarDespesaDto);
   }
@@ -18,7 +29,9 @@ export class DespesasController {
   }
 
   @Delete('/:_id')
-  async deletarDespesa(@Param('_id') _id: string): Promise<void> {
+  async deletarDespesa(
+    @Param('_id', DespesasValidacaoPipe) _id: string,
+  ): Promise<void> {
     this.despesasService.deletarDespesa(_id);
   }
 }
--- src/despesas/dtos/criar-atualizar-despesa.dto.ts
+++ src/despesas/dtos/criar-atualizar-despesa.dto.ts
@@ -1,8 +1,14 @@
 import { tipoDespesa } from '../types/tipoDespesa';
-
+import { IsDate, IsEnum, IsNotEmpty, IsNumber } from 'class-validator';
 export class CriarAtualizarDespesaDto {
+  @IsNotEmpty()
   description: string;
+  @IsNotEmpty()
+  @IsNumber()
   value: number;
+  @IsEnum(tipoDespesa)
+  @IsNotEmpty()
   type: tipoDespesa;
+  @IsDate()
   dataDespesa: Date;
 }
